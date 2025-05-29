27856a1781f8f4b09cc7c59582de2969782e352d
Amanda Persdotter <amer1301@student.miun.se>
2025-05-29 15:41:50 +0200

    hamburgermeny för admin

--- menu-nav.js
+++ menu-nav.js
@@ -1,8 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // För besökare
   const toggleBtn = document.querySelector('.menu-toggle');
-  const menuNav = document.querySelector('.menu-nav');
+  const menuNav = document.querySelector('.menu-nav:not(.admin-menu)'); // Välj bara besökarmenyn
 
-  toggleBtn.addEventListener('click', () => {
-    menuNav.classList.toggle('open');
-  });
+  if (toggleBtn && menuNav) {
+    toggleBtn.addEventListener('click', () => {
+      menuNav.classList.toggle('open');
+    });
+  }
+
+  // För admin
+  const adminToggle = document.querySelector('.menu-toggle-admin');
+  const adminMenu = document.querySelector('.admin-menu');
+
+  if (adminToggle && adminMenu) {
+    adminToggle.addEventListener('click', () => {
+      adminMenu.classList.toggle('open-admin');
+    });
+  }
 });
